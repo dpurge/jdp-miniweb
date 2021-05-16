fff874d6fbd1455b1a1f9b145cbcdca9a0fbc52c
David P <david.purge@gmail.com>
2021-05-16 02:43:59 +0200

    Text editor

--- src/markdown-edit/js/main.js
+++ src/markdown-edit/js/main.js
@@ -72,97 +72,15 @@ function setIme(elem, ime) {
 
 function clearForm()
 {
-	phrase.value = "";
-	grammar.value = "";
-	transcription.value = "";
-	translation.value = "";
-	notes.value = "";
-}
-
-function renderVocabulary()
-{
-	vocabulary.innerHTML = "";
-	for (let i of data) {
-		_item = vocabulary.appendChild(
-			document.createElement('div'));
-			
-		_phrase = _item.appendChild(
-			document.createElement('span'));
-		_phrase.setAttribute('class', 'phrase');
-		_phrase.innerHTML = i.phrase;
-		
-		_item.innerHTML += " ";
-			
-		_grammar = _item.appendChild(
-			document.createElement('span'));
-		_grammar.setAttribute('class', 'grammar');
-		_grammar.innerHTML = i.grammar;
-		
-		_item.innerHTML += " ";
-		
-		_transcription = _item.appendChild(
-			document.createElement('span'));
-		_transcription.setAttribute('class', 'transcription');
-		_transcription.innerHTML = i.transcription;
-		
-		_item.innerHTML += " ";
-		
-		_translation = _item.appendChild(
-			document.createElement('span'));
-		_translation.setAttribute('class', 'translation');
-		_translation.innerHTML = i.translation;
-		
-		_item.innerHTML += " ";
-		
-		_notes = _item.appendChild(
-			document.createElement('span'));
-		_notes.setAttribute('class', 'notes');
-		_notes.innerHTML = i.notes;
-	}
-}
-
-function getData() {
-
-	var buffer = "Phrase\tGrammar\tTranscription\tTranslation\tNotes\n";
-	for (let i of data) {
-		buffer += i.phrase;
-		buffer += "\t";
-		buffer += i.grammar;
-		buffer += "\t";
-		buffer += i.transcription;
-		buffer += "\t";
-		buffer += i.translation;
-		buffer += "\t";
-		buffer += i.notes;
-		buffer += "\n";
-	}
-
-	return buffer;
-}
-
-function onAdd(event)
-{
-	event.preventDefault();
-	var item = {
-		phrase: phrase.value,
-		grammar: grammar.value,
-		transcription: transcription.value,
-		translation: translation.value,
-		notes: notes.value
-	}
-	
-	data.push(item);
-	clearForm();
-	renderVocabulary();
-	
-	phrase.focus();
+	document.getElementById('tekst').value = "";
+	tekst.focus();
 }
 
 function onDownload(event)
 {
 	event.preventDefault();
 
-	var buffer = getData();
+	var buffer = document.getElementById('tekst').value;
 	
 	const blobData = new Blob(
 		[ buffer ], 
@@ -180,14 +98,14 @@ function onDownload(event)
 	
 	link.click();
 	URL.revokeObjectURL(blobUrl);
-	phrase.focus();
+	tekst.focus();
 }
 
 async function onCopy(event)
 {
 	event.preventDefault();
 
-	var buffer = getData();
+	var buffer = tekst.value;
 	
 	if (!navigator.clipboard) {
 		return;
@@ -198,22 +116,14 @@ async function onCopy(event)
 	} catch (error) {
 		console.error("Copy to clipboard failed", error);
 	}
-}
-
-function onUndo(event)
-{
-	event.preventDefault();
-	clearForm();
+	tekst.focus();
 }
 
 function onReset(event)
 {
 	event.preventDefault();
 	clearForm();
-	
-	data = [];
-	renderVocabulary();
-	phrase.focus();
+	tekst.focus();
 }
 
 function sortIme(a,b) {
@@ -221,23 +131,14 @@ function sortIme(a,b) {
 }
 
 function onLoad()
-{	
-	window.data = [];
-	
-	add.onclick = onAdd;
+{
 	download.onclick = onDownload;
 	coppy.onclick = onCopy;
-	undo.onclick = onUndo;
 	resset.onclick = onReset;
 	
-	if (typeof phraseIme !== 'undefined') {
-		phraseIme.sort(sortIme);
-		setIme(phrase, phraseIme);
-	}
-	
-	if (typeof transcriptionIme !== 'undefined') {
-		transcriptionIme.sort(sortIme);
-		setIme(transcription, transcriptionIme);
+	if (typeof IME !== 'undefined') {
+		IME.sort(sortIme);
+		setIme(tekst, IME);
 	}
 }
 
